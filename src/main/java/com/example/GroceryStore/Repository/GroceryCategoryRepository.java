69e945a1f6bc6d46f0864417944bce606f23e661
Dinesh Amaranadh <doddaka.dinesh@gmail.com>
2024-08-27 20:10:24 +0530

    Removed files after moving to grocery-store branch

--- src/main/java/com/example/GroceryStore/Repository/GroceryCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.GroceryStore.Repository;
-
-
-import com.example.GroceryStore.Entity.GroceryCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroceryCategoryRepository extends JpaRepository<GroceryCategory, Integer> {
-}
-
